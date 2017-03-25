48b88b3529e7130c2f29ca4d098fc07ce89cdd54
Guy Gur-Ari <guy@gurari.net>
2017-03-25 09:44:29 +0000

    script that lists the public urls

---
+++ get-http-urls.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/env perl
+# 
+# Get the list of hep-th URLs on our S3 bucket.
+#
+
+use strict;
+use warnings;
+use IO::File;
+
+my $in = IO::File->new("s3cmd ls s3://hep-th |");
+my $out = IO::File->new("> hep-th-urls.txt");
+
+while (<$in>) {
+    chomp;
+    s|.*s3://|https://s3.amazonaws.com/|;
+    $out->print("$_\n");
+}
+
+$in->close();
+$out->close();
+
