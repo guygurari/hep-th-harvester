6c5ff45709a8195efb4ab048889911f358435ad6
Guy Gur-Ari <guy@gurari.net>
2017-03-26 06:44:37 +0000

    utilities

---
+++ metadata/lookup-paper.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use DBI;
+use FindBin qw($Script);
+use Data::Dumper;
+
+my $dbfile = 'hep-th.sqlite';
+
+my $dbh = DBI->connect("dbi:SQLite:dbname=$dbfile","","") || die;
+
+sub usage {
+    print "Usage: $Script arxiv-id\n\n";
+}
+
+if (scalar(@ARGV) != 1) {
+    usage();
+    exit 1;
+}
+
+my $id = $ARGV[0];
+
+my $sth = $dbh->prepare("SELECT * FROM papers where id='$id'");
+$sth->execute();
+my $row = $sth->fetch();
+
+if (defined $row) {
+    print Dumper($row);
+}
+else {
+    print "Paper $id not found\n";
+}
+
