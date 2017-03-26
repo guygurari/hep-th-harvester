82a35a561a75e96bba64b4c69105a2b319639bcb
Guy Gur-Ari <guy@gurari.net>
2017-03-25 19:12:58 -0700

    trying to harvest arxiv metadata

---
+++ metadata/harvest-arxiv-metadata.py
@@ -0,0 +1,107 @@
+#!/usr/bin/python
+
+import os.path
+import time
+import urllib2
+import datetime
+#from itertools import ifilter
+#from collections import Counter, defaultdict
+import xml.etree.ElementTree as ET
+
+#from bs4 import BeautifulSoup
+#import matplotlib.pylab as plt
+import pandas as pd
+#import numpy as np
+#import bibtexparser
+import sqlite3 as sql
+
+#pd.set_option('mode.chained_assignment','warn')
+
+OAI = "{http://www.openarchives.org/OAI/2.0/}"
+ARXIV = "{http://arxiv.org/OAI/arXiv/}"
+category = "physics:hep-th"
+filename = "hep-th.pkl"
+
+if os.path.isfile(filename):
+    df = pd.read_pickle(filename)
+    start_date = df.tail(1)["datestamp"]
+else:
+    df = pd.DataFrame(columns=("oai_id", "title", "datestamp", "abstract", "categories", "created", "id", "doi", "comments"))
+    start_date = '1980-01-01'
+
+print start_date
+exit(0)
+
+def harvest(df, arxiv):
+    base_url = "http://export.arxiv.org/oai2?verb=ListRecords&"
+    url = (base_url +
+           "from=%s&until=2010-12-31&" +
+           "metadataPrefix=arXiv&set=%s" % (start_date, arxiv))
+    
+    while True:
+        print "fetching", url
+        try:
+            response = urllib2.urlopen(url)
+            
+        except urllib2.HTTPError, e:
+            if e.code == 503:
+                to = int(e.hdrs.get("retry-after", 30))
+                print "Got 503. Retrying after {0:d} seconds.".format(to)
+
+                time.sleep(to)
+                continue
+                
+            else:
+                raise
+            
+        xml = response.read()
+
+        root = ET.fromstring(xml)
+
+        for record in root.find(OAI+'ListRecords').findall(OAI+"record"):
+            header = record.find(OAI+'header')
+            meta = record.find(OAI+'metadata')
+            info = meta.find(ARXIV+"arXiv")
+            created = info.find(ARXIV+"created").text
+            created = datetime.datetime.strptime(created, "%Y-%m-%d")
+            categories = info.find(ARXIV+"categories").text
+
+            # if there is more than one DOI use the first one
+            # often the second one (if it exists at all) refers
+            # to an eratum or similar
+            doi = info.find(ARXIV+"doi")
+            if doi is not None:
+                doi = doi.text.split()[0]
+
+            authors = info.find(ARXIV+"authors")
+            if authors is not None:
+                for author in authors.findall(ARXIV+'author'):
+                    
+                
+            contents = {'oai_id': header.find(OAI+'identifier').text
+                        'title': info.find(ARXIV+"title").text,
+                        'datestamp': header.find(OAI+'datestamp').text,
+                        'id': info.find(ARXIV+"id").text,#oai_id.text[4:],
+                        'abstract': info.find(ARXIV+"abstract").text.strip(),
+                        'created': created,
+                        'categories': categories.split(),
+                        'comments': info.find(ARXIV+"comments").text,
+                        'doi': doi,
+                        }
+
+            df = df.append(contents, ignore_index=True)
+            df.to_pickle(filename)
+
+        # The list of articles returned by the API comes in chunks of
+        # 1000 articles. The presence of a resumptionToken tells us that
+        # there is more to be fetched.
+        token = root.find(OAI+'ListRecords').find(OAI+"resumptionToken")
+        if token is None or token.text is None:
+            break
+
+        else:
+            url = base_url + "resumptionToken=%s"%(token.text)
+            
+    return df
+
+harvest(df, category)
