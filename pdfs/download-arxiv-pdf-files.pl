c225c1dea96102d7cace2b7e572307176db5a1a6
Guy Gur-Ari <guy@gurari.net>
2017-04-04 16:40:44 -0700

    re-download arxiv pdf chunks

--- pdfs/download-arxiv-pdf-files.pl
+++ pdfs/download-arxiv-pdf-files.pl
@@ -50,13 +50,13 @@ while (my $line = <$chunk_list_file>) {
         print ">>> Chunk $chunk_url done, skipping\n\n";
         next;
     }
-    elsif (-f $chunk_file) {
-        print ">>> Chunk file already exists\n\n";
-    }
-    else {
-        print ">>> Downloading $chunk_url ...\n\n";
-        execute("$s3 get --requester-pays $chunk_url");
-    }
+    #elsif (-f $chunk_file) {
+        #print ">>> Chunk file already exists, deleting\n\n";
+		#unlink $chunk_file;
+    #}
+
+	print ">>> Downloading $chunk_url ...\n\n";
+	execute("$s3 get --requester-pays $chunk_url");
 
     die "Missing file $chunk_file" unless -f $chunk_file;
 
@@ -80,7 +80,7 @@ while (my $line = <$chunk_list_file>) {
         if (Arxiv::is_pdf_in_category($pdf, $category)) {
 			if ($upload_to_s3) {
 				print " in $category, uploading\n";
-				execute("s3cmd --acl-public --quiet --no-mime-magic put $pdf $upload_bucket_url",
+				execute("$s3 --acl-public --quiet --no-mime-magic put $pdf $upload_bucket_url",
 					    $num_upload_retries);
 				unlink $pdf || die;
 			}
